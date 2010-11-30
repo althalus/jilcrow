c3db607a61d167bf40a2fd9f8aed3d12c7daed6b
Steve Losh <steve@stevelosh.com>
2010-11-30 17:24:49 -0500

    Add a setup.py
    --HG--
    rename : README => README.markdown
    rename : jilcrow.py => bin/jilcrow

---
+++ README.markdown
@@ -0,0 +1,18 @@
+JILCROW
+=======
+
+A static site generator, tailored for *my* own needs. (Work in progress.)
+
+Use It
+------
+
+    mkdir mysite
+    cd mysite
+    mkvirtualenv mysite
+    pip install -e http://bitbucket.org/sjl/jilcrow/#egg=jilcrow
+
+    ... make your folders ...
+
+    jilcrow .
+
+Licensed under the terms of the MIT license (see the LICENSE file).
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README')
+description = 'A small static site generator that uses Jinja templates.'
+
+if os.path.exists(README_PATH):
+    long_description = open(README_PATH).read()
+else:
+    long_description = description
+
+setup(
+    name='jilcrow',
+    version='0.1.0',
+    install_requires=['BeautifulSoup', 'python-dateutil', 'Jinja2', 'Markdown',
+                      'PyRSS2Gen', 'PyYAML'],
+    description=description,
+    long_description=long_description,
+    author='Liam Cooke, Steve Losh',
+    url='http://bitbucket.org/sjl/jilcrow/',
+    packages=['jilcrow'],
+    scripts=['bin/jilcrow'],
+)
+
